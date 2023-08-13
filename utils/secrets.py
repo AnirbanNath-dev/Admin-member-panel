7e97faf774b087d8aa236b4bf501934046d3c708
/AnIrBaN <116329986+AnirbanNath-dev@users.noreply.github.com>
2023-08-13 20:54:43 +0530

    Update secrets.py

--- utils/secrets.py
+++ utils/secrets.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+try:
+    USERNAME = os.environ.get('NAME')
+    PASSWORD = os.environ.get('PASSWORD')
+except:
+    USERNAME = 'ExampleUser'
+    PASSWORD = 'ExamplePassword'
 
-USERNAME = os.environ.get('NAME')
-PASSWORD = os.environ.get('PASSWORD')
